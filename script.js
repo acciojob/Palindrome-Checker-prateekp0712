9fd247fba14c5a143f863bccf2d280ef601765db
prateekp0712 <37222674+prateekp0712@users.noreply.github.com>
2023-01-11 21:59:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
+	let polishedString = str.replace(\/W+/g, "");
+	let reversedString = polishedString.split("").reversed().join("");
+	console.log(reversedString,polishedString)
+	if(polishedString != reversedString){
+		return false;
+	}
+	return true;
 
 }
 module.exports = palindrome
